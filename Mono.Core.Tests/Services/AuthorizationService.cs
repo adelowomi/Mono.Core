b01b4b0ddc8014905a965b56f6c0beb12cbff964
weyinmi-dev <weyinmi.e.madamedon@gmail.com>
2024-07-30 02:51:19 +0100

    AuthorizationService-Tests

---
+++ Mono.Core.Tests/Services/AuthorizationService.cs
@@ -0,0 +1,93 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using Xunit;
+using Mono.Core;
+using Mono.Core.Authorization;
+using Mono.Core.Accounts;
+using Refit;
+
+namespace Mono.Core.Tests
+{
+    public class AuthorizationServiceTests
+    {
+        private readonly Mock<IAuthorizationService> _mockAuthorizationService;
+        private readonly AuthorizationService _authorizationService;
+
+        public AuthorizationServiceTests()
+        {
+            _mockAuthorizationService = new Mock<IAuthorizationService>();
+            var refitClientBuilder = new Mock<IRefitClientBuilder<IAuthorizationService>>();
+            refitClientBuilder.Setup(x => x.Build()).Returns(_mockAuthorizationService.Object);
+            _authorizationService = new AuthorizationService(refitClientBuilder.Object);
+        }
+
+        [Fact]
+        public async Task InitiateAccountLinking_ShouldReturnExpectedResponse()
+        {
+            // Arrange
+            var accountLinkingModel = new AccountLinkingModel();
+            var expectedResponse = new MonoStandardResponse<AccountLinkingResponseModel>();
+            _mockAuthorizationService.Setup(x => x.InitiateAccountLinking(It.IsAny<AccountLinkingModel>(), It.IsAny<CancellationToken>()))
+                                     .ReturnsAsync(new ApiResponse<MonoStandardResponse<AccountLinkingResponseModel>>(new HttpResponseMessage(), expectedResponse, null));
+
+            // Act
+            var result = await _authorizationService.InitiateAccountLinking(accountLinkingModel);
+
+            // Assert
+            Assert.Equal(expectedResponse, result);
+            _mockAuthorizationService.Verify(x => x.InitiateAccountLinking(accountLinkingModel, It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task AuthorizeAccount_ShouldReturnExpectedResponse()
+        {
+            // Arrange
+            var authorizationAccountRequestModel = new AuthorizationAccountRequestModel { Code = "test-code" };
+            var expectedResponse = new MonoStandardResponse<AuthorizationAccountResponseModel> ();
+            _mockAuthorizationService.Setup(x => x.AuthorizeAccount(It.IsAny<AuthorizationAccountRequestModel>(), It.IsAny<CancellationToken>()))
+                                     .ReturnsAsync(new ApiResponse<MonoStandardResponse<AuthorizationAccountResponseModel>>(new HttpResponseMessage(), expectedResponse, null));
+
+            // Act
+            var result = await _authorizationService.AuthorizeAccount(authorizationAccountRequestModel);
+
+            // Assert
+            Assert.Equal(expectedResponse, result);
+            _mockAuthorizationService.Verify(x => x.AuthorizeAccount(authorizationAccountRequestModel, It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task SyncAccount_ShouldReturnExpectedResponse()
+        {
+            // Arrange
+            var accountId = "test-account-id";
+            var expectedResponse = new MonoStandardResponse<ManualDataSyncResponseModel> ();
+            _mockAuthorizationService.Setup(x => x.SyncAccount(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+                                     .ReturnsAsync(new ApiResponse<MonoStandardResponse<ManualDataSyncResponseModel>>(new HttpResponseMessage(), expectedResponse, null));
+
+            // Act
+            var result = await _authorizationService.SyncAccount(accountId);
+
+            // Assert
+            Assert.Equal(expectedResponse, result);
+            _mockAuthorizationService.Verify(x => x.SyncAccount(accountId, It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task ReauthorizeAccount_ShouldReturnExpectedResponse()
+        {
+            // Arrange
+            var accountId = "test-account-id";
+            var expectedResponse = new MonoStandardResponse<ReAuthorizationCodeResponseModel>();
+            _mockAuthorizationService.Setup(x => x.ReauthorizeAccount(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+                                     .ReturnsAsync(new ApiResponse<MonoStandardResponse<ReAuthorizationCodeResponseModel>>(new HttpResponseMessage(), expectedResponse, null));
+
+            // Act
+            var result = await _authorizationService.ReauthorizeAccount(accountId);
+
+            // Assert
+            Assert.Equal(expectedResponse, result);
+            _mockAuthorizationService.Verify(x => x.ReauthorizeAccount(accountId, It.IsAny<CancellationToken>()), Times.Once);
+        }
+    }
+}
